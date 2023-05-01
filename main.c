31110a2159f619d145d91fd28b8edd7f341f8331
Furkan Safa Altunyuva <furkansafaaltunyuva@gmail.com>
2023-05-01 16:12:31 +0300

    Solution to Infinite Loop with fgets()

--- main.c
+++ main.c
@@ -13,57 +13,45 @@ struct Node {
 void printList(struct Node* n)
 {
     while (n != NULL) {
-        printf("%d %d %d \n", n->user_id, n->item_id, n->rating);
+        printf("User ID:%d Item ID:%d Rating:%d \n", n->user_id, n->item_id, n->rating);
         n = n->next;
     }
 }
 
 void deleteNode(struct Node** head_ref, int par_user_id, int par_item_id)
 {
-    // Store head node
     struct Node *temp = *head_ref, *prev;
 
-    // If head node itself holds the key to be deleted
     if (temp != NULL && temp->user_id == par_user_id && temp->item_id == par_item_id) {
-        *head_ref = temp->next; // Changed head
-        free(temp); // free old head
+        *head_ref = temp->next;
+        free(temp);
         return;
     }
 
-    // Search for the key to be deleted, keep track of the
-    // previous node as we need to change 'prev->next'
     while (temp != NULL && temp->user_id != par_user_id && temp->item_id != par_item_id) {
         prev = temp;
         temp = temp->next;
     }
 
-    // If key was not present in linked list
     if (temp == NULL)
         return;
 
-    // Unlink the node from linked list
     prev->next = temp->next;
 
-    free(temp); // Free memory
+    free(temp);
 }
 
 void append(struct Node** head_ref, int par_user_id, int par_item_id, int par_rating)
 {
-    /* 1. allocate node */
     struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+    struct Node *last = *head_ref;
 
-    struct Node *last = *head_ref;  /* used in step 5*/
-
-    /* 2. put in the data  */
     new_node->user_id  = par_user_id;
     new_node->item_id  = par_item_id;
     new_node->rating  = par_rating;
 
-    /* 3. This new node is going to be the last node, so make next
-          of it as NULL*/
     new_node->next = NULL;
 
-    /* 4. If the Linked List is empty, then make the new node as head */
     if (*head_ref == NULL)
     {
         *head_ref = new_node;
@@ -71,11 +59,9 @@ void append(struct Node** head_ref, int par_user_id, int par_item_id, int par_ra
         return;
     }
 
-    /* 5. Else traverse till the last node */
     while (last->next != NULL)
         last = last->next;
 
-    /* 6. Change the next of last node */
     last->next = new_node;
 
     printf("Customer rating (%d, %d) is added succesful \n", par_user_id, par_item_id);
@@ -97,6 +83,7 @@ int average(struct Node** head_ref, int par_item_id){
         prev = temp;
         temp = temp->next;
     }
+
     if(item_id_found == 1){
         return sum/node_count;
     }
@@ -135,7 +122,6 @@ int main()
     int item_id;
     int rating;
 
-
     while (1) {
         scanf("%19[^\n]", &input);
         char *p = strtok(input, " ");
@@ -152,20 +138,27 @@ int main()
             sscanf(array[2], "%d", &item_id);
             sscanf(array[3], "%d", &rating);
             append(&head, user_id, item_id, rating);
+            fgets(input, 20, stdin);
         }
         else if(strcmp(array[0], "RATING") == 0){
             sscanf(array[1], "%d", &user_id);
             sscanf(array[2], "%d", &item_id);
             rating_function(&head, user_id, item_id);
+            fgets(input, 20, stdin);
         }
         else if(strcmp(array[0], "AVERAGE") == 0){
             sscanf(array[1], "%d", &item_id);
             average(&head, item_id);
+            fgets(input, 20, stdin);
         }
         else if(strcmp(array[0], "REMOVE") == 0){
             sscanf(array[1], "%d", &user_id);
             sscanf(array[2], "%d", &item_id);
             deleteNode(&head, user_id, item_id);
+            fgets(input, 20, stdin);
+        }
+        else if(strcmp(array[0], "BREAK") == 0){
+            break;
         }
 
         /*
